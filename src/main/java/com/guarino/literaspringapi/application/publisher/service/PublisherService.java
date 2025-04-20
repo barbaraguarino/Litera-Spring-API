c15452a9b270e7c2a3893abb7719db69ccc8eaec
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-19 23:38:01 -0300

    Criação da função para salvar uma nova editora no sistema.

---
+++ src/main/java/com/guarino/literaspringapi/application/publisher/service/PublisherService.java
@@ -0,0 +1,39 @@
+package com.guarino.literaspringapi.application.publisher.service;
+
+import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
+import com.guarino.literaspringapi.application.publisher.dto.PublisherResponseDTO;
+import com.guarino.literaspringapi.application.publisher.mapper.PublisherMapper;
+import com.guarino.literaspringapi.domain.publisher.repository.PublisherRepository;
+
+import com.guarino.literaspringapi.shared.exception.ResourceAlreadyExistsException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PublisherService {
+
+    private final PublisherRepository publisherRepository;
+    private final PublisherMapper publisherMapper;
+
+    public PublisherService(PublisherRepository publisherRepository, PublisherMapper publisherMapper) {
+        this.publisherRepository = publisherRepository;
+        this.publisherMapper = publisherMapper;
+    }
+
+    public PublisherResponseDTO createPublisher(PublisherRequestDTO request) {
+
+        if(!request.email().isEmpty() && publisherRepository.existsByEmail(request.email().toUpperCase()))
+            throw new ResourceAlreadyExistsException("Publisher", "email",  request.email());
+
+        if(publisherRepository.existsByNameAndFoundationDateAndCountryHeadquarter(request.name().toUpperCase(),
+                request.foundationData().toUpperCase(), request.countryHeadquarter().toUpperCase()))
+            throw new ResourceAlreadyExistsException("Publisher", "dados");
+
+        var publisher = publisherMapper.toEntity(request);
+        publisher = publisherRepository.save(publisher);
+
+        return publisherMapper.toResponseDTO(publisher);
+    }
+
+}
