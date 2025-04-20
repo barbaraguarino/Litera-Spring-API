69e12c13490d5339847dbdef03e06ad6b7c6f8be
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-20 18:38:41 -0300

    Adição do @JsonFormat para o timestamp de erro.

--- src/main/java/com/guarino/literaspringapi/shared/dto/ErrorResponseDTO.java
+++ src/main/java/com/guarino/literaspringapi/shared/dto/ErrorResponseDTO.java
@@ -1,5 +1,6 @@
 package com.guarino.literaspringapi.shared.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public class ErrorResponseDTO {
 
     private String errorType;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private String timestamp;
     private String className;
     private String message;
