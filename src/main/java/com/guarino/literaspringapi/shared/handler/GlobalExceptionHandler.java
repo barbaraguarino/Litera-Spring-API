fffb91f603149786c09388acf61ef3ece08109b4
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-20 16:45:17 -0300

    Adição do MethodArgumentNotValidException no controle de exceções.

--- src/main/java/com/guarino/literaspringapi/shared/handler/GlobalExceptionHandler.java
+++ src/main/java/com/guarino/literaspringapi/shared/handler/GlobalExceptionHandler.java
@@ -4,24 +4,51 @@ import com.guarino.literaspringapi.shared.dto.ErrorResponseDTO;
 import com.guarino.literaspringapi.shared.exception.ResourceAlreadyExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponseDTO> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        StringBuilder errorMessage = new StringBuilder();
+
+        for (FieldError fieldError : fieldErrors) {
+            errorMessage.append(fieldError.getField())
+                    .append(": ")
+                    .append(fieldError.getDefaultMessage())
+                    .append("\n");
+        }
 
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
-                "ResourceAlreadyExistsException",
-                ex.getClass().getName(),
-                ex.getMessage()
+                "Erro de validação.",
+                "MethodArgumentNotValidException",
+                errorMessage.toString().trim()
         );
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(errorResponse);
     }
+
+    @ExceptionHandler(ResourceAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleResourceAlreadyExistsException(ResourceAlreadyExistsException ex) {
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                "Recurso já cadastrado.",
+                "ResourceAlreadyExistsException",
+                ex.getMessage()
+        );
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(errorResponse);
+    }
 }
+
 
