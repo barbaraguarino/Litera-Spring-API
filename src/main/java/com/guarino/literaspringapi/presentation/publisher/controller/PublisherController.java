019e5cbb42d9c8754f9b110e827517fcf6c77f98
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-19 23:39:33 -0300

    Creation of the endpoint to save a new publisher in the database.

---
+++ src/main/java/com/guarino/literaspringapi/presentation/publisher/controller/PublisherController.java
@@ -0,0 +1,28 @@
+package com.guarino.literaspringapi.presentation.publisher.controller;
+
+import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
+import com.guarino.literaspringapi.application.publisher.dto.PublisherResponseDTO;
+import com.guarino.literaspringapi.application.publisher.service.PublisherService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/publisher")
+public class PublisherController {
+
+    private final PublisherService publisherService;
+
+    public PublisherController(PublisherService publisherService) {
+        this.publisherService = publisherService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<PublisherResponseDTO> createPublisher(@RequestBody @Valid PublisherRequestDTO request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(publisherService.createPublisher(request));
+    }
+}
