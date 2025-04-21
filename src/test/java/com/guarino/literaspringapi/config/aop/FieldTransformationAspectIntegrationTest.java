c9d9e2b5d9139329eae963dff4d60985f2d570c9
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 19:49:18 -0300

    Correction of formatting.

--- src/test/java/com/guarino/literaspringapi/config/aop/FieldTransformationAspectIntegrationTest.java
+++ src/test/java/com/guarino/literaspringapi/config/aop/FieldTransformationAspectIntegrationTest.java
@@ -3,6 +3,7 @@ package com.guarino.literaspringapi.config.aop;
 import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
 import com.guarino.literaspringapi.application.publisher.mapper.PublisherMapper;
 import com.guarino.literaspringapi.domain.publisher.entity.Publisher;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
