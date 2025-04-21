a79bca6ba1f44fe31d7f8d2070075e53449ee4d2
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 18:52:12 -0300

    Addition of more test cases.

--- src/test/java/com/guarino/literaspringapi/presentation/publisher/controller/PublisherControllerIntegrationTest.java
+++ src/test/java/com/guarino/literaspringapi/presentation/publisher/controller/PublisherControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.guarino.literaspringapi.presentation.publisher.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
 import com.guarino.literaspringapi.application.publisher.service.PublisherService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -31,6 +32,8 @@ class PublisherControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private PublisherRequestDTO request;
+
     @TestConfiguration
     static class MockConfig {
         @Bean
@@ -39,21 +42,29 @@ class PublisherControllerIntegrationTest {
         }
     }
 
+    @BeforeEach
+    void setUp() {
+        request = new PublisherRequestDTO(
+                "Editora Abril",
+                "1950-03-04",
+                "A Editora Abril é uma das maiores editoras de revistas do Brasil, " +
+                        "com publicações em diversos segmentos, incluindo revistas de notícias, " +
+                        "entretenimento e estilo de vida.",
+                "contato@abril.com.br",
+                "https://www.abril.com.br",
+                "02183757000193",
+                "1134567890"
+        );
+
+    }
+
     @Nested
     class CreatePublisher{
 
         @Test
-        @DisplayName("Deve lançar uma exceção de validação quando o nome for nulo ou vazio")
-        void shouldThrowValidationExceptionWhenNameIsNullOrEmpty() throws Exception {
-            PublisherRequestDTO request = new PublisherRequestDTO(
-                    "",
-                    "2025-04-20",
-                    "Descrição válida.",
-                    "email@dominio.com",
-                    "https://www.google.com",
-                    "123456789",
-                    "12345678901234"
-            );
+        @DisplayName("Deve lançar uma exceção de validação quando o nome for vazio")
+        void shouldThrowValidationExceptionWhenNameIsEmpty() throws Exception {
+            request.setName("");
 
             mockMvc.perform(post("/api/publisher")
                             .contentType(MediaType.APPLICATION_JSON)
@@ -65,24 +76,86 @@ class PublisherControllerIntegrationTest {
         }
 
         @Test
-        @DisplayName("Deve lançar uma exceção de validação quando o e-mail for inválido")
-        void shouldThrowValidationExceptionWhenEmailIsInvalid() throws Exception {
-            PublisherRequestDTO requestWithInvalidEmail = new PublisherRequestDTO(
-                    "Nome da Editora",
-                    "2025-04-20",
-                    "Descrição válida.",
-                    "email-inválido",  // Email inválido
-                    "https://www.google.com",
-                    "123456789",
-                    "12345678901234"
-            );
+        @DisplayName("Deve lançar uma exceção de validação quando email for nulo")
+        void shouldThrowValidationExceptionWhenNameIsNull() throws Exception {
+            request.setName(null);
 
             mockMvc.perform(post("/api/publisher")
                             .contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(requestWithInvalidEmail)))
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorType").value("Erro de validação."))
+                    .andExpect(jsonPath("$.messages").isArray())
+                    .andExpect(jsonPath("$.messages[?(@ =~ /.*nome.*/i)]").exists());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção de validação quando o nome tiver menos de 3 caracteres.")
+        void shouldThrowValidationExceptionWhenNameHasLessThanThreeCharacters() throws Exception {
+            request.setName("S3");
+
+            mockMvc.perform(post("/api/publisher")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorType").value("Erro de validação."))
+                    .andExpect(jsonPath("$.messages").isArray())
+                    .andExpect(jsonPath("$.messages[?(@ =~ /.*nome.*/i)]").exists());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção de validação quando data não tiver a formação desejada.")
+        void shouldThrowValidationExceptionWhenDateFormatIsInvalid() throws Exception {
+            request.setFoundationDate("25-10-2025");
+
+            mockMvc.perform(post("/api/publisher")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorType").value("Erro de validação."))
+                    .andExpect(jsonPath("$.messages").isArray())
+                    .andExpect(jsonPath("$.messages[?(@ =~ /.*foundationDate.*/i)]").exists());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção de validação quando telefone tiver mais de 15 caracteres.")
+        void shouldThrowValidationExceptionWhenPhoneNumberExceedsMaxLength() throws Exception {
+            request.setTelephone("1234567891234567891");
+
+            mockMvc.perform(post("/api/publisher")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorType").value("Erro de validação."))
+                    .andExpect(jsonPath("$.messages").isArray())
+                    .andExpect(jsonPath("$.messages[?(@ =~ /.*telephone.*/i)]").exists());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção de validação quando o e-mail for inválido")
+        void shouldThrowValidationExceptionWhenEmailIsInvalid() throws Exception {
+            request.setEmail("emaildominio.com");
+
+            mockMvc.perform(post("/api/publisher")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
                     .andExpect(status().isBadRequest())
                     .andExpect(jsonPath("$.errorType").value("Erro de validação."))
                     .andExpect(jsonPath("$.messages[?(@ =~ /.*email.*/i)]").exists());
+        }
+
+        @Test
+        @DisplayName("Deve lançar uma exceção de validação quando telefone tiver caracteres diferentes de números.")
+        void shouldThrowValidationExceptionWhenPhoneNumberHasNonNumericCharacters() throws Exception {
+            request.setTelephone("125-548");
+
+            mockMvc.perform(post("/api/publisher")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.errorType").value("Erro de validação."))
+                    .andExpect(jsonPath("$.messages").isArray())
+                    .andExpect(jsonPath("$.messages[?(@ =~ /.*telephone.*/i)]").exists());
         }
 
         @Test
