b941ca4e23974929b3d038f1e4137d27945e8898
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 19:05:19 -0300

    Addition of all checks after the transformation.

--- src/test/java/com/guarino/literaspringapi/application/publisher/service/PublisherServiceIntegrationTest.java
+++ src/test/java/com/guarino/literaspringapi/application/publisher/service/PublisherServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package com.guarino.literaspringapi.application.publisher.service;
 
 import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
 import com.guarino.literaspringapi.application.publisher.dto.PublisherResponseDTO;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,18 +20,26 @@ class PublisherServiceIntegrationTest {
     @Test
     void shouldCreatePublisherWithTransformedFields() {
         PublisherRequestDTO request = new PublisherRequestDTO(
-                " Nome da Editora ",
-                "2025-04-20",
-                "   Descrição válida.   ",
-                "email@dominio.com",
-                "https://www.google.com",
-                "123456789",
-                "12345678901234"
+                "Editora Abril   ",
+                "1950-03-04",
+                "   A Editora Abril é uma das maiores editoras de revistas do Brasil, " +
+                        "com publicações em diversos segmentos, incluindo revistas de notícias, " +
+                        "entretenimento e estilo de vida.   ",
+                "  contato@abril.com.br",
+                "https://www.abril.com.br  ",
+                " 02183757000193",
+                "1134567890 "
         );
 
         PublisherResponseDTO response = publisherService.createPublisher(request);
 
-        assertEquals("NOME DA EDITORA", response.name());
-        assertEquals("Descrição válida.", response.description());
+        assertEquals("EDITORA ABRIL", response.name());
+        assertEquals("A Editora Abril é uma das maiores editoras de revistas do Brasil, " +
+                "com publicações em diversos segmentos, incluindo revistas de notícias, " +
+                "entretenimento e estilo de vida.", response.description());
+        assertEquals("CONTATO@ABRIL.COM.BR", response.email());
+        assertEquals("HTTPS://WWW.ABRIL.COM.BR", response.website());
+        assertEquals("02183757000193", response.taxId());
+        assertEquals("1134567890", response.telephone());
     }
 }
