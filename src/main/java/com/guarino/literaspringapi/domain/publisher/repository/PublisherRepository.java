53e026573817364cbd50c02fcec7adc5ee8c87ec
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 19:26:03 -0300

    Change in the formatting.

--- src/main/java/com/guarino/literaspringapi/domain/publisher/repository/PublisherRepository.java
+++ src/main/java/com/guarino/literaspringapi/domain/publisher/repository/PublisherRepository.java
@@ -1,10 +1,11 @@
 package com.guarino.literaspringapi.domain.publisher.repository;
 
 import com.guarino.literaspringapi.domain.publisher.entity.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, UUID> {
