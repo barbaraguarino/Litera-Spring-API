9a3a0ca52de7bc63ad833ea2580e0e00bfcbe931
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 19:32:20 -0300

    Removal of unused function.

--- src/main/java/com/guarino/literaspringapi/shared/util/StringUtils.java
+++ src/main/java/com/guarino/literaspringapi/shared/util/StringUtils.java
@@ -5,12 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class StringUtils {
 
-    public boolean isBlank(String value) {
-        return value == null || value.trim().isEmpty();
-    }
-
     public boolean isNotBlank(String value) {
         return value != null && !value.trim().isEmpty();
     }
-
 }
