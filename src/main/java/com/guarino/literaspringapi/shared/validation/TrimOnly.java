a85bd369cc749d82538ab9ac046d89ba51c723ae
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-21 19:33:51 -0300

    Change in the formatting.

--- src/main/java/com/guarino/literaspringapi/shared/validation/TrimOnly.java
+++ src/main/java/com/guarino/literaspringapi/shared/validation/TrimOnly.java
@@ -1,8 +1,8 @@
 package com.guarino.literaspringapi.shared.validation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
