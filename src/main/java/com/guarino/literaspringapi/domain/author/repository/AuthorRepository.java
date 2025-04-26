fb039822262c5ebdb966ff6513c0434e6167db92
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-26 17:13:10 -0300

    Creation of the function to check if the email already exists in the database.

--- src/main/java/com/guarino/literaspringapi/domain/author/repository/AuthorRepository.java
+++ src/main/java/com/guarino/literaspringapi/domain/author/repository/AuthorRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, UUID> {}
+public interface AuthorRepository extends JpaRepository<Author, UUID> {
+
+    boolean existsByEmail(String email);
+}
