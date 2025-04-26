4aed39b1a80d8845aebc3b9a2a914db93f747d04
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-26 17:14:06 -0300

    Completing the function to register the author in the database, without integration with AWS S3.

--- src/main/java/com/guarino/literaspringapi/application/author/service/AuthorService.java
+++ src/main/java/com/guarino/literaspringapi/application/author/service/AuthorService.java
@@ -2,23 +2,36 @@ package com.guarino.literaspringapi.application.author.service;
 
 import com.guarino.literaspringapi.application.author.dto.AuthorRequestDTO;
 import com.guarino.literaspringapi.application.author.dto.AuthorResponseDTO;
+import com.guarino.literaspringapi.application.author.mapper.AuthorMapper;
 import com.guarino.literaspringapi.domain.author.repository.AuthorRepository;
+import com.guarino.literaspringapi.shared.util.UniquenessValidator;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 @Service
 @Transactional
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
+    private final AuthorMapper authorMapper;
+    private final UniquenessValidator uniquenessValidator;
 
-    public AuthorService(AuthorRepository authorRepository) {
+    public AuthorService(AuthorRepository authorRepository, AuthorMapper authorMapper, UniquenessValidator uniquenessValidator) {
         this.authorRepository = authorRepository;
+        this.authorMapper = authorMapper;
+        this.uniquenessValidator = uniquenessValidator;
     }
 
     public AuthorResponseDTO createAuthor(@Valid AuthorRequestDTO request) {
-
-        return null;
+        var author = authorMapper.toEntity(request);
+        uniquenessValidator.validateMultiple("Autor(a)", Map.of(
+                "email", new UniquenessValidator.ValidationEntry(author.getEmail(),
+                        v -> authorRepository.existsByEmail((String) v))
+        ));
+        author = authorRepository.save(author);
+        return authorMapper.toResponseDTO(author);
     }
 }
