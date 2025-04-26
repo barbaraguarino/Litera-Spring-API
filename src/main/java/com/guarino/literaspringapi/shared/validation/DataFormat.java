c2b5135d5443089fc84985cd9bf5c227e2ea12b1
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-26 16:54:22 -0300

    Creation of the annotations.

---
+++ src/main/java/com/guarino/literaspringapi/shared/validation/DataFormat.java
@@ -0,0 +1,15 @@
+package com.guarino.literaspringapi.shared.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.constraints.Pattern;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "A data deve estar no formato yyyy-MM-dd.")
+public @interface DataFormat {}
---
+++ src/main/java/com/guarino/literaspringapi/shared/validation/OnlyLetters.java
@@ -0,0 +1,15 @@
+package com.guarino.literaspringapi.shared.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.constraints.Pattern;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {})
+@Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ\\s]+$", message = "O campo deve conter apenas letras e espaços.")
+public @interface OnlyLetters {}
