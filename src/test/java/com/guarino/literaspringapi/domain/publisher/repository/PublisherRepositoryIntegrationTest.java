360399e9dfab9bc995d3d039e25ab5af133d1ec7
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-04-26 14:59:54 -0300

    Update PublisherRepositoryIntegrationTest.java

--- src/test/java/com/guarino/literaspringapi/domain/publisher/repository/PublisherRepositoryIntegrationTest.java
+++ src/test/java/com/guarino/literaspringapi/domain/publisher/repository/PublisherRepositoryIntegrationTest.java
@@ -2,20 +2,22 @@ package com.guarino.literaspringapi.domain.publisher.repository;
 
 import com.guarino.literaspringapi.application.publisher.dto.PublisherRequestDTO;
 import com.guarino.literaspringapi.domain.publisher.entity.Publisher;
+import com.guarino.literaspringapi.shared.validation.CaseId;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.guarino.literaspringapi.shared.validation.CaseId;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 
